e587aada258ceec300d726f1fcd282151e51ecfe
leejua1 <1479q@naver.com>
2020-08-12 11:16:18 +0900

    0812

--- src/teacher/streaming/TeacherStreaming.js
+++ src/teacher/streaming/TeacherStreaming.js
@@ -90,17 +90,24 @@ export class TeacherStreaming extends Component{ //필요한것... 수업코드,
             })
         this.setState({nowPageProps:  this.state.videoProps.slice(0,6)})
 
-
         this.socket.emit('joinRoom', {roomName : this.state.classCode, code: this.state.teacherCode}) //state에 저장된 classCode와 teacherCode가 서버로 보내진다.
-
 
         this.socket.on('letOffer',data=>{
             console.log('receive start offer message from server')
             let {peer1, localStream} = this.state
             peer1 = new RTCPeerConnection(this.state.config)
             navigator.mediaDevices.getUserMedia({video : true})
-                .then(stream=>{stream.getTracks().forEach(track => peer1.addTrack(track,localStream))})
-            peer1.onicecandidate =  e => {this.iceCandidateHandler(e)}
+                .then(stream=>{stream.getTracks().forEach(track => peer1.addTrack(track,stream))})
+            peer1.onicecandidate =  e => {
+                console.log(`caller send icecandidate message to 100018002`)
+                if (e.candidate){
+                    this.sendMessage({
+                        type : "candidate",
+                        target : "100018002",
+                        candidate : e.candidate
+                    })
+                }
+            }
             peer1.ontrack = e=>  {this.setRemoteTrack(e)}
             this.setState({peer1, localStream})
             this.offer(data)
